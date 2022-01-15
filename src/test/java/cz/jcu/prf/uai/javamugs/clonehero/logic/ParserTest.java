4d151d75825ebdb98e6ecbd9751da25409da9e37
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2022-01-15 19:31:44 +0000

    - split up tests

--- src/test/java/cz/jcu/prf/uai/javamugs/clonehero/logic/ParserTest.java
+++ src/test/java/cz/jcu/prf/uai/javamugs/clonehero/logic/ParserTest.java
@@ -1,28 +1,27 @@
 package cz.jcu.prf.uai.javamugs.clonehero.logic;
 
-import junit.framework.TestCase;
+import junit.framework.*;
 import java.io.IOException;
 
 public class ParserTest extends TestCase
 {
   private static final String TEST_CHARTS_PATH = "./tracks/testCharts/";
-  private static final String VALID_FILE_PATH = TEST_CHARTS_PATH + "ValidChart.prc";
-  private static final String MISSING_EXTENSION_PATH = TEST_CHARTS_PATH + "MissingExtension";
-  private static final String TOO_LARGE_FILE_PATH = TEST_CHARTS_PATH + "TooLarge.prc";
-  private static final String DROPPING_EARLY_PRESSES_PATH = TEST_CHARTS_PATH + "DroppingEarlyPresses.prc";
-  private static final String UNEXPECTED_FORMAT_PATH = TEST_CHARTS_PATH + "WrongFormat.prc";
   private static final double TIME_OFFSET = 3000;
 
-  public void testParseFile() throws Exception
+  public void testParseFile_VALID_FILE_PATH() throws Exception
   {
-    PressChart chart;
-    Parser parser = new Parser();
-
-    chart = parser.parseFile(VALID_FILE_PATH, TIME_OFFSET);
+    final String VALID_FILE_PATH = TEST_CHARTS_PATH + "ValidChart.prc";
+    var parser = new Parser();
+    var chart = parser.parseFile(VALID_FILE_PATH, TIME_OFFSET);
     assertNotNull(chart);
+  }
 
+  public void testParseFile_MISSING_EXTENSION_PATH() throws Exception
+  {
+    final String MISSING_EXTENSION_PATH = TEST_CHARTS_PATH + "MissingExtension";
     try
     {
+      var parser = new Parser();
       parser.parseFile(MISSING_EXTENSION_PATH, TIME_OFFSET);
       fail();
     }
@@ -33,9 +32,14 @@ public class ParserTest extends TestCase
         fail();
       }
     }
+  }
 
+  public void testParseFile_TOO_LARGE_FILE_PATH() throws Exception
+  {
+    final String TOO_LARGE_FILE_PATH = TEST_CHARTS_PATH + "TooLarge.prc";
     try
     {
+      var parser = new Parser();
       parser.parseFile(TOO_LARGE_FILE_PATH, TIME_OFFSET);
       fail();
     }
@@ -46,12 +50,22 @@ public class ParserTest extends TestCase
         fail();
       }
     }
+  }
 
-    chart = parser.parseFile(DROPPING_EARLY_PRESSES_PATH, TIME_OFFSET);
+  public void testParseFile_DROPPING_EARLY_PRESSES_PATH() throws Exception
+  {
+    final String DROPPING_EARLY_PRESSES_PATH = TEST_CHARTS_PATH + "DroppingEarlyPresses.prc";
+    var parser = new Parser();
+    var chart = parser.parseFile(DROPPING_EARLY_PRESSES_PATH, TIME_OFFSET);
     assertTrue(chart.next(50000).isEmpty());
+  }
 
+  public void testParseFile_UNEXPECTED_FORMAT_PATH() throws Exception
+  {
+    final String UNEXPECTED_FORMAT_PATH = TEST_CHARTS_PATH + "WrongFormat.prc";
     try
     {
+      var parser = new Parser();
       parser.parseFile(UNEXPECTED_FORMAT_PATH, TIME_OFFSET);
       fail();
     }
