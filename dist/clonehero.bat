53009ea1ae8a5c218f85f6f76e33de3933bbf2c7
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2022-01-15 19:04:29 +0000

    - update to v1.1.0

--- dist/clonehero.bat
+++ dist/clonehero.bat
@@ -1,1 +1,1 @@
- java --module-path %PATH_TO_FX% --add-modules javafx.controls,javafx.fxml,javafx.media  -jar clonehero-1.0.2.jar
+ java --module-path %PATH_TO_FX% --add-modules javafx.controls,javafx.fxml,javafx.media  -jar clonehero-1.1.0.jar
